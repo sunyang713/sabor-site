760a8106a773b0073d70ab96c84540eff2b392cb
Jonathan <sunyang713@hotmail.com>
2016-01-15 03:37:22 -0500

    Install new dependencies for future. Huge webpack config update. Delete .babelrc and move index.html to assets (it's a template).

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,76 @@
-'use strict';
-
+"use strict";
 
 /* EXTREMELY IMPORTANT HERE!!! */
-var publicPath = '/cu/sabor/'
+var publicPath = "/cu/sabor/"; // trailing slash needed?
 
-var webpack = require('webpack'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  path = require('path'),
-  srcPath = path.join(__dirname, 'src');
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var srcPath = path.join(__dirname, "src");
+var inDevMode = process.env.NODE_ENV === "dev"
 
 module.exports = {
-  target: 'web',
+  target: "web",
   cache: true,
   entry: {
-    common: ['react', 'react-dom', 'react-bootstrap', 'react-router', 'react-router-bootstrap', 'moment', 'react-addons-create-fragment'],//, 'alt'] // this thing is freaking 5mb
-    index: path.join(srcPath, 'index.js')
-  },
-  resolve: {
-    root: srcPath,
-    extensions: ['', '.js', '.jsx', '.styl'],
-    modulesDirectories: ['node_modules', 'src']
+    common: inDevMode ? [
+      "react",
+      "react-dom",
+      "react-bootstrap",
+      "react-router",
+      "react-router-bootstrap",
+      "moment",
+      "react-addons-create-fragment"
+    ] : [
+      "react-lite",
+      "react-bootstrap",
+      "react-router",
+      "react-router-bootstrap",
+      "moment"
+    ],
+    index: path.join(srcPath, "index.js")
   },
   output: {
-    path: path.join(__dirname, 'dist' + publicPath),
+    path: path.join(__dirname, "dist"),
     publicPath: publicPath,
-    filename: '[name].js',
-    library: ['Example', '[name]'],
-    pathInfo: true
+    filename: "[name].js",
+    pathInfo: inDevMode
   },
   module: {
     loaders: [
-      // required to write "require('./style.css')"
+      // required for js
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "stage-0", "react"]
+        }
+      },
+
+      // required for react jsx
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "stage-0", "react"]
+        }
+      },
+
+      // required to write "require("./style.css")"
       { test: /\.css$/, exclude: /\.useable\.css$/, loader: "style-loader!css-loader" },
 
       // Stylus loader
-      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
 
       // required for bootstrap icons.
-      //  url-loader uses DataUrls. 
-      //  file-loader emits files. 
-      { test: /\.(woff|woff2)$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      //  url-loader uses DataUrls.
+      //  file-loader emits files.
+      { test: /\.(woff|woff2)$/, loader: "url-loader?prefix=font/&limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf$/, loader: "file-loader?prefix=font/" },
       { test: /\.eot$/, loader: "file-loader?prefix=font/" },
       { test: /\.svg$/, loader: "file-loader?prefix=font/" },
-
-      // required for react jsx
-      { test: /\.js$/, exclude: /(node_modules)/, loader: "babel-loader" },
-      { test: /\.jsx$/, exclude: /(node_modules)/, loader: "babel-loader" },
 
       // misc
       { test: /\.json$/, loader: "json-loader" },
@@ -54,28 +78,66 @@ module.exports = {
       { test: /\.jpg$/, loader: "file-loader" }
     ]
   },
+  resolve: {
+    alias: inDevMode ? {
+      // Bind version of jquery
+      jquery: "jquery-2.1.4",
+
+      // Bind version of jquery-ui
+      "jquery-ui": "jquery-ui-1.10.5",
+
+      // jquery-ui doesn"t contain a index file
+      // bind module to the complete module
+      "jquery-ui-1.10.5$": "jquery-ui-1.10.5/ui/jquery-ui.js"
+    } : {
+      "react": "react-lite",
+      "react-dom": "react-lite",
+
+      // Bind version of jquery
+      jquery: "jquery-2.1.4",
+
+      // Bind version of jquery-ui
+      "jquery-ui": "jquery-ui-1.10.5",
+
+      // jquery-ui doesn"t contain a index file
+      // bind module to the complete module
+      "jquery-ui-1.10.5$": "jquery-ui-1.10.5/ui/jquery-ui.js"
+    },
+    root: srcPath,
+    extensions: ["", ".js", ".jsx"],
+    modulesDirectories: ["node_modules", "src"]
+  },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+    new webpack.optimize.CommonsChunkPlugin("common", "common.js"),
     new HtmlWebpackPlugin({
-      inject: true,
-      template: 'src/index.html'
+      title: "Sabor",
+      inject: "body",
+      // favicon: path.join(srcPath, "assets/favicon.ico"),
+      template: path.join(srcPath, "assets/index.html")
     }),
     new webpack.NoErrorsPlugin(),
     new webpack.ProvidePlugin({
         $: "jquery",
         jQuery: "jquery",
         "window.jQuery": "jquery"
+    }),
+    new webpack.DefinePlugin({
+      "env.PROD": process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production",
+      "env.PRODUCTION": process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production",
+      "env.DEV": process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "development",
+      "env.DEVELOPMENT": process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "development",
+      "env.SOURCE": process.env.NODE_ENV === "dev" ? JSON.stringify("http://localhost:5000") : JSON.stringify("http://www.columbia.edu/cu/sabor")
     })
   ],
+  // some janky fix for 'request' npm package
   node: {
     net: "empty",
     tls: "empty"
   },
-
-  debug: true,
-  devtool: 'eval-cheap-module-source-map',
+  debug: inDevMode,
+  devtool: inDevMode ? "source-map" : "cheap-module-source-map",
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     historyApiFallback: true
   }
 };
