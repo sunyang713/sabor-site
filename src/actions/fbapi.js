0f2ea2aa69d8d34c4f65d728766345a37b8b7cdb
Jonathan <sunyang713@hotmail.com>
2016-01-24 13:25:48 -0500

    Transform keychain into a fruit basket. Use 'apple' and 'bananas.'

--- src/actions/fbapi.js
+++ src/actions/fbapi.js
@@ -1,10 +1,10 @@
-import { FB_APP_ID, FB_TOKEN } from 'KEYCHAIN'
+import { APPLE, BANANA } from 'KEYCHAIN'
 import merge from 'toolbox/merge'
 
-export function fbapi({ url, fields, additional, callback }) {
+export function fbapi({ url, fields, additional = {}, callback }) {
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : FB_APP_ID,
+      appId      : APPLE,
       xfbml      : true,
       version    : 'v2.5'
     })
@@ -12,14 +12,21 @@ export function fbapi({ url, fields, additional, callback }) {
       url,
       'GET',
       merge({
-        access_token: FB_TOKEN,
+        access_token: BANANA,
         'fields': fields
       }, additional),
       function(response) {
         callback(response);
       }
     )
-  }
+  };
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 }
 
 
