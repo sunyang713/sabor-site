8f0d0c6edd634f000e69cd768cbddd8880569601
Jonathan <sunyang713@hotmail.com>
2016-01-23 23:59:13 -0500

    Minor skeleton tags to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,20 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>
-          <a href="http://jonathansun.me">{ 'Site designed by Jonathan' }</a>
-          { '&copy; 2015 Sabor &middot; ' }
-          <a href="https://www.facebook.com/cusabor">{ 'facebook' }</a>
-          { ' &middot; ' }
-          <a href="http://twitter.com/SaborTweets">{ 'twitter' }</a>
-        </p>
+        <div className="container">
+          <p>
+            <div className="u-pull-left">
+              &copy;{ ' 2015 Sabor ' }
+              &middot;
+              <a href="https://www.facebook.com/cusabor">{ ' facebook ' }</a>
+              &middot;
+              <a href="http://twitter.com/SaborTweets">{ ' twitter ' }</a>
+            </div>
+            <div className="u-pull-right">
+              <a href="http://jonathansun.me">{ 'Site designed by Jonathan' }</a>
+            </div>
+          </p>
+        </div>
       </footer>
     )
   }
