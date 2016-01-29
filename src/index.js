c751c371485e199aef70a7701462af51c98483a9
Jonathan <sunyang713@hotmail.com>
2016-01-29 17:59:32 -0500

    Nothing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-//TODO INSTLALL ESLINT THINGS
-
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Redirect, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux'
 import createHistory from 'history/lib/createHashHistory'
-
 import configureStore from 'store/configureStore'
-
-// TODO: use fbgraph, add like/share buttons
 
 /* Pages */
 import Home from 'pages/Home'
 import Team from 'pages/Team'
 import Releve from 'pages/Releve'
 import NotFound from 'pages/NotFound'
-
 
 /* Global styles */
 import 'assets/css/normalize.css'
