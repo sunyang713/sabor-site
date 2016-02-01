dc978492053e9577695c9e376184ec6a4b65da05
Jonathan <sunyang713@hotmail.com>
2016-01-31 22:31:54 -0500

    Delete <hr>'s around navbar.

--- src/pages/Releve.jsx
+++ src/pages/Releve.jsx
@@ -10,9 +10,7 @@ export default class Releve extends React.Component {
   render() {
     return (
       <div>
-        <hr />
         <Navbar />
-        <hr />
         <h1>{ 'Releve' }</h1>
         <Footer />
 
