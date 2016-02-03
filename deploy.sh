5b1611f090284f86e9cbbc4b96c888905973bb8b
Jonathan <sunyang713@hotmail.com>
2016-02-03 00:42:27 -0500

    Update travis script. Copy deploy script to root.

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+DIST="dist/"
+TARGET="~/public_html/"
+USER="jys2124"
+
+chmod +x "dist/index.js"
+chmod +x "dist/common.js"
+# Transfer files
+expect -c "
+   spawn bash -c \"scp $DIST* $USER@cunix.cc.columbia.edu:$TARGET\"
+   expect password: { send $PASSWORD\r }
+   expect eof
+"
+echo "\nThank you for your patronage. Have a nice day.\n"
+
