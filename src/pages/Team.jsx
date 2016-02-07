6c556c077165dde9399b0b919514aab3431d5424
Jonathan <sunyang713@hotmail.com>
2016-02-07 02:16:31 -0500

    Nothing.

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 /* Containers */
 import Navbar from 'containers/Navbar'
-// import TeamList from 'containers/TeamList'
 
+/* Components */
 import BoardList from 'components/BoardList'
 import TeamList from 'components/TeamList'
 import Footer from 'components/Footer'
