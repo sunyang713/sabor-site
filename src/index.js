926f3536b68294ea3389f809f1a28b94696db8a9
Jonathan <sunyang713@hotmail.com>
2016-02-11 14:02:42 -0500

    Remove unused imports.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 /* global __DEV__ */
 import React from 'react'
 import { render } from 'react-dom'
-import createHistory from 'history/lib/createHashHistory'
-import { Provider } from 'react-redux'
-import configureStore from 'store/configureStore'
 
 import ROOT from 'containers/Root'
 
