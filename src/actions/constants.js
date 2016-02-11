b96c3c73969aefb39655e6f00afecd006177d2b8
Jonathan <sunyang713@hotmail.com>
2016-02-11 14:01:50 -0500

    Remove old unused files.

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,23 +1,3 @@
-export const SHOW_MODAL = 'SHOW_MODAL'
-export const CLOSE_MODAL = 'CLOSE_MODAL'
-export const SET_MODAL_CONTENT = 'SET_MODAL_CONTENT'
-export const SET_MODAL_TITLE = 'SET_MODAL_TITLE'
-export const SET_MODAL_IMG_SRC = 'SET_MODAL_IMG_SRC'
-export const SET_MODAL_INFO = 'SET_MODAL_INFO'
-export const SET_MODAL_DESCRIPTION = 'SET_MODAL_DESCRIPTION'
-
-export const ADD_EVENT = 'ADD_EVENT'
-export const ADD_EVENTS = 'ADD_EVENTS'
-export const SET_EVENTS = 'SET_EVENTS'
-
-export const STORE = 'STORE'
-
-export const SET_IMAGE = 'SET_IMAGE'
-export const INCREMENT = 'INCREMENT'
-export const RESET = 'RESET'
-
-export const NEXT = 'NEXT'
-
 export const SET_LATEST_EVENT = 'SET_LATEST_EVENT'
 export const SET_EXISTING = 'SET_EXISTING'
 
