d6352974af87586d45ce5bece8a868aec20c23c7
Jonathan <sunyang713@hotmail.com>
2017-05-05 04:17:55 -0400

    Make facebook api calls simple http requests.
    This kills the preflight requests and other possible hiccups.

--- src/utils/fbapi.js
+++ src/utils/fbapi.js
@@ -3,12 +3,7 @@ import jsonToQueryString from 'utils/jsonToQueryString'
 export default function fbapi (path, queryParams, callback) {
   return fetch(
     `https://graph.facebook.com/v2.8/${ path }?${ jsonToQueryString(queryParams) }&access_token=${ process.env.FACEBOOK_ACCESS_TOKEN }`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
+    { method: 'GET' }
   ).then(
     response => response.json()
   ).then(
