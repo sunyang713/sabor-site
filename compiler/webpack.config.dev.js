715b395557631a97a939c5bf6aad09b40d1691bd
Jonathan <sunyang713@hotmail.com>
2017-05-06 16:58:56 -0400

    HMR...

--- compiler/webpack.config.dev.js
+++ compiler/webpack.config.dev.js
@@ -2,15 +2,18 @@ var webpack = require('webpack')
 
 module.exports = {
   output: {
+    filename: 'js/[name].[hash].js',
     pathinfo: true
   },
   devServer: {
     historyApiFallback: true,
     overlay: true,
-    stats: 'minimal'
+    stats: 'minimal',
+    hot: true
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
-    new webpack.NamedModulesPlugin()
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
