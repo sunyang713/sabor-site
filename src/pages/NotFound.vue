2477a3181ee8335323fd098b1f85054e44a80402
Jonathan <sunyang713@hotmail.com>
2017-08-30 20:33:45 -0700

    Give more meaningful 'Not Found' page content.

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <h4>notfound</h4>
+  <div id="not-found-page">
+    <div class="jumbotron">
+      <div class="container">
+        <h4 class="display-4">The page you're looking for does not exist!</h4>
+        <p class="lead">The address may be incorrect, or the page may still be under construction. If this is an error, please e-mail us at sabor (dot) board (atsign) gmail (dot) com</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'not-found-page'
 }
 </script>
 
